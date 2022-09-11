5e6e3d0d94343b695671ffb4aeabc190bb761164
loganwe <91347950+loganwe@users.noreply.github.com>
2022-09-11 15:40:11 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,9 +1,15 @@
 input.onButtonPressed(Button.A, function () {
-    in_num += 1
-    basic.showString("" + (in_num))
+    
+    if(ended==true){
+        control.reset()
+    }else{
+        in_num += 1
+        basic.showString("" + (in_num))
+    }
 })
 input.onButtonPressed(Button.AB, function () {
     start()
+
 })
 input.onButtonPressed(Button.B, function () {
     in_num += 0 - 1
@@ -11,23 +17,26 @@ input.onButtonPressed(Button.B, function () {
 })
 function start () {
     time = in_num * 1000
-    while (!(time == 0)) {
-        basic.showString("" + (time / 1000))
-        time += -1000
-        if (time == 1000) {
-            while (loop) {
-                basic.showIcon(IconNames.Yes)
-                basic.showLeds(`
-                    . . . . .
-                    . . . . .
-                    . . . . .
-                    . . . . .
-                    . . . . .
-                    `)
-            }
+    while(loop&&!ended){
+    basic.showString("" + (time / 1000))
+    time += -1000
+    if (time==1000) {
+        ended = true
+        while (loop&&ended) {
+            basic.showIcon(IconNames.Yes)
+            basic.showLeds(`
+                . . . . .
+                . . . . .
+                . . . . .
+                . . . . .
+                . . . . .
+                `)
         }
     }
+    }
 }
+
+let ended = false
 let time = 0
 let in_num = 0
 let loop = false
